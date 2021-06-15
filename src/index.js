db483cca8c97079fa115a3e356e1d54c3aad85b8
Shaher-11 <shahershamroukh@gmail.com>
2021-06-15 15:20:30 +0200

    Add home section

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import navbar from './navbar';
+import myHome from './myHome';
 
 const body = document.getElementsByTagName('body')[0];
 body.prepend(navbar());
+const contentDiv = document.getElementById('content');
+contentDiv.append(myHome());
